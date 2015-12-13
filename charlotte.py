157a25bf33a1669c19c5036f65b15b0a9a1a47b3
Sergey Goder <sgoder@fb.com>
2015-12-12 16:08:30 -0800

    Add some randomness

--- charlotte.py
+++ charlotte.py
@@ -10,6 +10,105 @@ from math import sin, cos
 from PIL import Image, ImageDraw
 
 
+class CharlottesWeb(object):
+    """ Generates an image of a spider's web. """
+
+    def __init__(self):
+        self.im_scale = 4
+        self.final_width = 1200
+        self.final_height = 1200
+        self.width = self.final_width * self.im_scale
+        self.height = self.final_height * self.im_scale
+        self.center = (self.width / 2, self.height / 2)
+        self.web_lines = []
+        self.num_circles = 75
+        self.im = Image.new(
+            'RGBA',
+            (self.width, self.height),
+            (20, 20, 20, 0),
+        )
+        self.draw = ImageDraw.Draw(self.im)
+
+    def render(self):
+        self.draw_web_lines()
+        self.draw_web_circles()
+        self.im.resize((self.final_width, self.final_height))
+        self.im.show()
+
+    def draw_web_circle(self, offset):
+        # iterate over adjacent pairs of lines
+        for i in range(len(self.web_lines)):
+            line1 = self.web_lines[i]
+            line2 = self.web_lines[(i + 1) % len(self.web_lines)]
+            p1_positive, p1_negative = self.get_offset_points(line1, offset)
+            p2_positive, p2_negative = self.get_offset_points(line2, offset)
+
+            # There are two sets of lines we could draw, always draw lines that
+            # are shorter
+            off_line_positive_1 = Line(p1_positive, p2_positive)
+            off_line_positive_2 = Line(p1_positive, p2_negative)
+            off_line_negative_1 = Line(p1_negative, p2_positive)
+            off_line_negative_2 = Line(p1_negative, p2_negative)
+
+            # Sometimes don't draw the line
+            if random.random() < 0.01:
+                return
+
+            if off_line_positive_1.length < off_line_positive_2.length:
+                self.draw_line(off_line_positive_1)
+                self.draw_line(off_line_negative_2)
+            else:
+                self.draw_line(off_line_positive_2)
+                self.draw_line(off_line_negative_1)
+
+    def get_offset_points(self, line, offset):
+        p1 = self.trans_to_cartesian(line.p1)
+        p2 = self.trans_to_cartesian(line.p2)
+        cart_line = Line(p1, p2)
+        x_offset = math.sqrt((offset ** 2) / (1 + cart_line.slope ** 2))
+        y_offset = x_offset * cart_line.slope
+        return (
+            self.trans_to_pil(Point(x_offset, y_offset)),
+            self.trans_to_pil(Point(-x_offset, -y_offset)),
+        )
+
+    def draw_web_circles(self):
+        growth_rate = 1.5
+        min_offset = math.log(1)
+        max_offset = math.log(self.height * growth_rate * 10)
+        log_space = np.linspace(min_offset, max_offset, num=self.num_circles)
+        random_var = np.array(
+            [random.random() / 10 for _ in range(self.num_circles)],
+        )
+        for log_off in (log_space + random_var):
+            offset = math.exp(log_off / growth_rate) * max_offset
+            # import ipdb; ipdb.set_trace()
+            self.draw_web_circle(offset)
+
+    def draw_web_lines(self):
+        num_lines = 8
+        for line in range(num_lines // 2):
+            start = Point(0, 1.5 * self.height // 2)
+            end = Point(0, -1.5 * self.height // 2)
+            theta = 2 * math.pi * (line / num_lines)
+            theta += (random.random() - 0.5) / 2
+            nstart = self.trans_to_pil(start.rotate(theta))
+            nend = self.trans_to_pil(end.rotate(theta))
+            web_line = Line(nstart, nend)
+
+            self.draw_line(web_line)
+            self.web_lines.append(web_line)
+
+    def draw_line(self, line):
+        self.draw.line(line.to_tuple(), fill=(220, 220, 210))
+
+    def trans_to_cartesian(self, point):
+        return Point(point.x - self.width / 2, point.y - self.height / 2)
+
+    def trans_to_pil(self, point):
+        return Point(point.x + self.width / 2, point.y + self.height / 2)
+
+
 class Point(object):
     def __init__(self, x, y):
         self.x = x
@@ -48,100 +147,10 @@ class Line(object):
         )
 
     def __repr__(self):
-        return 'Line({}, {})'.format(self.p1.__repr__(), self.p2.__repr__())
+        return 'Line({}, {})'.format(repr(self.p1), repr(self.p2))
 
     def to_tuple(self):
         return self.p1.to_tuple() + self.p2.to_tuple()
-
-
-class CharlottesWeb(object):
-    def __init__(self):
-        self.im_scale = 4
-        self.final_width = 1200
-        self.final_height = 1200
-        self.width = self.final_width * self.im_scale
-        self.height = self.final_height * self.im_scale
-        self.center = (self.width / 2, self.height / 2)
-        self.web_lines = []
-        self.num_circles = 75
-        self.im = Image.new(
-            'RGBA',
-            (self.width, self.height),
-            (20, 20, 20, 0),
-        )
-        self.draw = ImageDraw.Draw(self.im)
-
-    def render(self):
-        self.draw_web_lines()
-        self.draw_web_circles()
-        self.im.resize((self.final_width, self.final_height))
-        self.im.show()
-
-    def draw_web_circle(self, offset):
-        # iterate over adjacent pairs of lines
-        for i in range(len(self.web_lines)):
-            line1 = self.web_lines[i]
-            line2 = self.web_lines[(i + 1) % len(self.web_lines)]
-            p1_positive, p1_negative = self.get_offset_points(line1, offset)
-            p2_positive, p2_negative = self.get_offset_points(line2, offset)
-
-            # There are two sets of lines we could draw, always draw lines that
-            # are shorter
-            off_line_positive_1 = Line(p1_positive, p2_positive)
-            off_line_positive_2 = Line(p1_positive, p2_negative)
-            off_line_negative_1 = Line(p1_negative, p2_positive)
-            off_line_negative_2 = Line(p1_negative, p2_negative)
-
-            if off_line_positive_1.length < off_line_positive_2.length:
-                self.draw_line(off_line_positive_1)
-                self.draw_line(off_line_negative_2)
-            else:
-                self.draw_line(off_line_positive_2)
-                self.draw_line(off_line_negative_1)
-
-    def get_offset_points(self, line, offset):
-        p1 = self.trans_to_cartesian(line.p1)
-        p2 = self.trans_to_cartesian(line.p2)
-        cart_line = Line(p1, p2)
-        x_offset = math.sqrt((offset ** 2) / (1 + cart_line.slope ** 2))
-        y_offset = x_offset * cart_line.slope
-        return (
-            self.trans_to_pil(Point(x_offset, y_offset)),
-            self.trans_to_pil(Point(-x_offset, -y_offset)),
-        )
-
-    def draw_web_circles(self):
-        growth_rate = 1.5
-        min_offset = math.log(1)
-        max_offset = math.log(self.height * growth_rate * 10)
-        log_space = np.linspace(min_offset, max_offset, num=self.num_circles)
-        for log_off in log_space:
-            offset = math.exp(log_off / growth_rate) * max_offset
-            # import ipdb; ipdb.set_trace()
-            self.draw_web_circle(offset)
-
-    def draw_web_lines(self):
-        num_lines = 8
-        for line in range(num_lines // 2):
-            start = Point(0, 1.5 * self.height // 2)
-            end = Point(0, -1.5 * self.height // 2)
-            theta = 2 * math.pi * (line / num_lines)
-            theta += (random.random() - 0.5) / 2
-            nstart = self.trans_to_pil(start.rotate(theta))
-            nend = self.trans_to_pil(end.rotate(theta))
-            web_line = Line(nstart, nend)
-
-            self.draw_line(web_line)
-            self.web_lines.append(web_line)
-
-    def draw_line(self, line):
-        self.draw.line(line.to_tuple(), fill=(220, 220, 210))
-
-    def trans_to_cartesian(self, point):
-        return Point(point.x - self.width / 2, point.y - self.height / 2)
-
-    def trans_to_pil(self, point):
-        return Point(point.x + self.width / 2, point.y + self.height / 2)
 
 
 if __name__ == '__main__':
