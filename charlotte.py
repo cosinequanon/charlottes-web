d1d6c896795e1a23e7b23e4d84c2186a25159148
Sergey Goder <sgoder@fb.com>
2015-12-12 20:14:48 -0800

    Bezier stuff is working

--- charlotte.py
+++ charlotte.py
@@ -7,6 +7,7 @@ import random
 
 from math import sin, cos
 
+import numpy as np
 from PIL import Image, ImageDraw
 
 
@@ -14,14 +15,14 @@ class CharlottesWeb(object):
     """ Generates an image of a spider's web. """
 
     def __init__(self):
-        self.im_scale = 4
+        self.im_scale = 2
         self.final_width = 1200
         self.final_height = 1200
         self.width = self.final_width * self.im_scale
         self.height = self.final_height * self.im_scale
         self.center = (self.width / 2, self.height / 2)
         self.web_lines = []
-        self.num_circles = 75
+        self.num_circles = 50
         self.im = Image.new(
             'RGBA',
             (self.width, self.height),
@@ -50,16 +51,15 @@ class CharlottesWeb(object):
             off_line_negative_1 = Line(p1_negative, p2_positive)
             off_line_negative_2 = Line(p1_negative, p2_negative)
 
-            # Sometimes don't draw the line
             if random.random() < 0.01:
                 return
 
             if off_line_positive_1.length < off_line_positive_2.length:
-                self.draw_line(off_line_positive_1)
-                self.draw_line(off_line_negative_2)
+                self.draw_web_bezier(off_line_positive_1)
+                self.draw_web_bezier(off_line_negative_2)
             else:
-                self.draw_line(off_line_positive_2)
-                self.draw_line(off_line_negative_1)
+                self.draw_web_bezier(off_line_positive_2)
+                self.draw_web_bezier(off_line_negative_1)
 
     def get_offset_points(self, line, offset):
         p1 = self.trans_to_cartesian(line.p1)
@@ -78,10 +78,11 @@ class CharlottesWeb(object):
         max_offset = math.log(self.height * growth_rate * 10)
         log_space = np.linspace(min_offset, max_offset, num=self.num_circles)
         random_var = np.array(
-            [random.random() / 10 for _ in range(self.num_circles)],
+            # [0.5 for _ in range(self.num_circles)],
+            [0.5 * random.random() for _ in range(self.num_circles)],
         )
         for log_off in (log_space + random_var):
-            offset = math.exp(log_off / growth_rate) * max_offset
+            offset = math.exp(log_off / growth_rate) * max_offset + 10
             # import ipdb; ipdb.set_trace()
             self.draw_web_circle(offset)
 
@@ -102,6 +103,47 @@ class CharlottesWeb(object):
     def draw_line(self, line):
         self.draw.line(line.to_tuple(), fill=(220, 220, 210))
 
+    def draw_point(self, point):
+        self.draw.point(point.to_tuple(), fill=(250, 250, 250))
+
+    def draw_large_point(self, point, size=3, fill=(250, 250, 250)):
+        """ To debug things. """
+        p1 = point - size
+        p2 = point + size
+        self.draw.ellipse(
+            p1.to_tuple() + p2.to_tuple(),
+            fill=fill,
+        )
+
+    def draw_web_bezier(self, line):
+        A, C = line.get_points()
+        x_midpoint = A.x + C.x / 2
+        perp_y_inter = A.y - line.slope * A.x
+        B_no_offset = Point(x_midpoint, line.slope * A.x + perp_y_inter)
+        B_no_offset = (A + C) / 2
+
+        # if line.slope > 0:
+        if self.trans_to_cartesian(B_no_offset).x < 0:
+            direction = 1
+        else:
+            direction = -1
+
+        offset = line.length * 0.3 * direction * (random.random() + 1.5) / 16
+
+        y_inter = B_no_offset.y - (-1 / line.slope) * B_no_offset.x
+        B = Point(
+            B_no_offset.x + offset,
+            (-1 / line.slope) * (B_no_offset.x + offset) + y_inter
+        )
+
+        p_prev = C
+        for t in np.linspace(0, 1, 20):
+            p1 = A * t + (1 - t) * B
+            p2 = B * t + (1 - t) * C
+            p_final = p1 * t + (1 - t) * p2
+            self.draw_line(Line(p_prev, p_final))
+            p_prev = p_final
+
     def trans_to_cartesian(self, point):
         return Point(point.x - self.width / 2, point.y - self.height / 2)
 
@@ -113,6 +155,41 @@ class Point(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __add__(self, other):
+        if isinstance(other, Point):
+            x, y = other.x, other.y
+        else:
+            x = y = other
+        return Point(self.x + x, self.y + y)
+
+    def __sub__(self, other):
+        if isinstance(other, Point):
+            x, y = other.x, other.y
+        else:
+            x = y = other
+        return Point(self.x - x, self.y - y)
+
+    def __radd__(self, other):
+        return Point(self.x + other, self.y + other)
+
+    def __mul__(self, other):
+        return Point(self.x * other, self.y * other)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        return Point(self.x / other, self.y / other)
+
+    def __rtruediv__(self, other):
+        return self.__truediv__(other)
+
+    def __floordiv__(self, other):
+        return Point(self.x // other, self.y // other)
+
+    def __rfloordiv__(self, other):
+        return self.__floordiv__(other)
 
     def __repr__(self):
         return 'Point({}, {})'.format(self.x, self.y)
@@ -139,7 +216,7 @@ class Line(object):
         self.p2 = p2
         try:
             self.slope = (self.p2.y - self.p1.y) / (self.p2.x - self.p1.x)
-        except ZeroDevisionError:
+        except ZeroDivisionError:
             raise ValueError('No slope for a vertical line')
         self.intercept = self.p1.y - self.slope * self.p1.x
         self.length = math.sqrt(
@@ -151,6 +228,9 @@ class Line(object):
 
     def to_tuple(self):
         return self.p1.to_tuple() + self.p2.to_tuple()
+
+    def get_points(self):
+        return self.p1, self.p2
 
 
 if __name__ == '__main__':
